2a05c8cafa28ba822cc3ecfc06358d9803cb4624
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-12 14:49:03 +0000

    added more movies

---
+++ src/componenets/BrowseMovies.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react'
+
+class BrowseMovies extends Component {
+    
+}
+
+export default BrowseMovies
--- src/componenets/MovieRow.jsx
+++ src/componenets/MovieRow.jsx
@@ -1,59 +1,118 @@
-import { Component } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Component } from "react"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 
 class MovieRow extends Component {
   state = {
-      movies: null
+      movies: null,
+      movies2: null,
+      movies3: null
 
   };
 
   fetchMovies = async () => {
 
-    let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=harry%20potter`;
+    let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=harry%20potter`
     try {
-      const response = await fetch(url);
-
-      const data = await response.json();
-      console.log(data)
-      console.log("right after the fetch");
-
+      const response = await fetch(url)
+      const data = await response.json()
       if (response.ok) {
         this.setState(
            {
             movies: data.Search
           }
         )
-        console.log(this.state.movies.Search);
       } else {
-        console.log("inside the else of Fetch"); 
       }
     } catch (e) {
       console.log(e);
-      console.log("outside the try block");
     }
-  };
+  }
+
+  fetchMovies2 = async () => {
+
+    let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=home%20alone`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      if (response.ok) {
+        this.setState(
+           {
+            movies2: data.Search
+          }
+        )
+      } else {
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  fetchMovies3 = async () => {
+
+    let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=christmas`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      if (response.ok) {
+        this.setState(
+           {
+            movies3: data.Search
+          }
+        )
+      } else {
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   componentDidMount = async () => {
-    this.fetchMovies();
-
-  };
+    this.fetchMovies()
+    this.fetchMovies2()
+    this.fetchMovies3()
+  }
 
   render() {
     return (
       <>
       { this.state.movies &&
-       <Row className="mx-1 mb-4 justify-content-center">
-                {
-                    this.state.movies.map(movie => (
-                        <Col key={movie.imdbID} xs='12' sm='6' md='4' lg='3' className="mb-3">
-                                <img src={movie.Poster} className="w-100 img-fluid rounded images" alt="" />
-                        </Col>
-                    ))
-                }
-                </Row>
-  }
+       <Row className="mx-1 my-4 justify-content-center">
+            {
+                this.state.movies.map((movie, index) => (
+                    index < 4 &&
+                    <Col key={movie.imdbID} xs='12' sm='6' md='4' lg='3' className="mb-3">
+                            <img src={movie.Poster} className="w-100 img-fluid rounded images" alt="" />
+                    </Col>
+                ))
+            }
+        </Row>
+    }
+          { this.state.movies2 &&
+       <Row className="mx-1 my-4 justify-content-center">
+            {
+                this.state.movies2.map((movie, index) => (
+                    index < 4 &&
+                    <Col key={movie.imdbID} xs='12' sm='6' md='4' lg='3' className="mb-3">
+                            <img src={movie.Poster} className="w-100 img-fluid rounded images" alt="" />
+                    </Col>
+                ))
+            }
+        </Row>
+    }
+          { this.state.movies3 &&
+       <Row className="mx-1 my-4 justify-content-center">
+            {
+                this.state.movies3.map((movie, index) => (
+                    index < 4 &&
+                    <Col key={movie.imdbID} xs='12' sm='6' md='4' lg='3' className="mb-3">
+                            <img src={movie.Poster} className="w-100 img-fluid rounded images" alt="" />
+                    </Col>
+                ))
+            }
+        </Row>
+    }
       </>
     );
   }
