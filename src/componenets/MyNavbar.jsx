0206ffb3e081cfa2e99f9db9a659b7f72828e023
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-12 12:46:07 +0000

    navbar search working, need to work on ui

--- src/componenets/MyNavbar.jsx
+++ src/componenets/MyNavbar.jsx
@@ -1,88 +1,71 @@
 import React from 'react'
-import { Navbar, Nav, Form, NavItem } from 'react-bootstrap'
-import { Col, Container, Row, FormControl, Button } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import SearchResults from './SearchResults'
 
 class MyNavbar extends React.Component {
 
     state = {
-        searchQuery: ''
+        searchQuery: '',
+        showSearchresults: false,
+        data: null
+    }
+
+    handleSubmit = async e => {
+        e.preventDefault()
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${this.state.searchQuery}`)
+        const data = await response.json()
+        console.log(data)
+        this.setState({data: data})
+        this.setState({showSearchresults: true})
     }
 
     render() {
         return (
+            <>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                <Navbar.Brand>
+                    <a className="navbar-brand " href="/"><img className="netflix-logo" src="../assets/netflix_logo.png" alt=""/></a>
+                    </Navbar.Brand>
 
-<Navbar bg="dark" variant="dark" expand="lg">
-  <Navbar.Brand>
-    <a className="navbar-brand " href="#"><img className="netflix-logo" src="../assets/netflix_logo.png" alt=""/></a>
-    </Navbar.Brand>
+                    <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/">TV Shows<span className="sr-only">(current)</span></a></li>
+                            <li className="nav-item active"><a className="nav-link" href="/">Movies</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/">Recently Added</a></li>
+                            <li className="nav-item"><a className="nav-link" href="/">My List</a></li>
+                        </ul>
 
-    <div className="collapse navbar-collapse text-white" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">TV Shows<span class="sr-only">(current)</span></a></li>
-            <li className="nav-item active"><a className="nav-link" href="#">Movies</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Recently Added</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">My List</a></li>
-        </ul>
-
-        <div className="form-inline my-2 my-lg-0">
-        
-        <span className="mx-2 profile-type">Kids</span>
-        <i className="bi bi-bell-fill mx-2"></i>
-        
-        <img className="drop-down-avatar mr-2" src="../assets/avatar.png" alt=""/>
-
-
-    <Form inline>
-      <FormControl 
-      type="text"
-      placeholder="Search here"
-      value={this.state.searchQuery}
-      onChange={e => this.setState({ searchQuery: e.target.value })} />
-      <Button variant="dark"><i className="bi bi-search mx-2"></i></Button>
-    </Form>
-        </div>
-
-    </div>
-  
-</Navbar>
+                        <div className="form-inline my-2 my-lg-0">
+                        
+                            <span className="mx-2 profile-type">Kids</span>
+                            <i className="bi bi-bell-fill mx-2"></i>
+                            
+                            <img className="drop-down-avatar mr-2" src="../assets/avatar.png" alt=""/>
 
 
+                            <Form inline onSubmit={this.handleSubmit}>
+                                <FormControl 
+                                    type="text"
+                                    placeholder="Search here"
+                                    value={this.state.searchQuery}
+                                    onChange={e => this.setState({ searchQuery: e.target.value })} 
+                                />
+                                <Button type="submit" variant="dark"><i className="bi bi-search mx-2"></i></Button>
+                            </Form>
+                        </div>
 
+                    </div>
+                
+                </Navbar>
 
-
-
-
-
-
-
-/* <Container>
-<Row>
-    <Col>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Search</Form.Label>
-            <Form.Control
-                type="text"
-                placeholder="Search here"
-                value={this.state.searchQuery}
-                onChange={e => this.setState({ searchQuery: e.target.value })}
-            />
-        </Form.Group>
-    </Col>
-</Row>
-<Row>
-    {
-        this.props.books.filter(b => b.title.toLowerCase().includes(this.state.searchQuery)).map(b => (
-            <Col xs={3} key={b.asin} >
-                <SingleBook book={b} />
-            </Col>
-        ))
+                {this.state.showSearchresults && <SearchResults movies={this.state.data} searchQuery={this.state.searchQuery}/>}
+            </>
+        )
     }
-</Row>
-</Container> */
-)
-}
-
 }
 
 
